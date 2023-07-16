19cbb84ca1a1f441e98418103a2569717dda0484
ElbekDeveloper <normurodovelbek0133@gmail.com>
2023-07-16 15:07:46 +0500

    Add Calculation Logic

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-﻿
+﻿using System;
+
 namespace Calculator
 {
    class Program
    {
         static void Main()
         {
+            Console.WriteLine("Enter values: ");
+            Console.Write("First number: ");
+            string firstUserValue = Console.ReadLine();
+            Console.Write("Second number: ");
+            string secondUserValue = Console.ReadLine();
+            
+            Console.WriteLine("Converting values in progress...");
+            decimal firstNumber = Convert.ToDecimal(firstUserValue);
+            decimal secondNumber = Convert.ToDecimal(secondUserValue);
 
+            Console.WriteLine("Here are your results: ");
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
         }
    } 
 }
